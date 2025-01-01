860989fdd544deb548feb4c8245b267495bea3f6
Lyn Kotuby <lyn.kotuby@gmail.com>
2025-01-01 17:55:29 -0500

    Shift up frets, drop Es for B6 mode

--- script.rb
+++ script.rb
@@ -6,7 +6,7 @@ require "terminal-table"
 ALL_NOTES = ["E", "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#", "D", "D#"]
 
 # high to low, add 1 to index to get to a string #
-TUNING = ["F#", "C#", "G#", "E", "B", "G#", "F#", "E", "B", "G#", "E", "B"]
+TUNING = ["F#", "C#", "G#", "D#", "B", "G#", "F#", "D#", "B", "G#", "E", "B"]
 
 RELATIONS = [
   "Root",
@@ -31,25 +31,48 @@ RELATIONS = [
   "13"
 ]
 
+
+def shift_key(to:)
+  ALL_NOTES
+    .slice_before(to)
+    .to_a # rails .split() would avoid the type fixup
+    .reverse
+    .flatten
+end
+
+def shift_tuning(key:, fret:)
+  TUNING.map do |note|
+    current_index = key.find_index(note)
+    new_index = (current_index + fret) % key.length
+    key[new_index]
+  end
+end
+
 def relation(key:, note:)
   interval = key.find_index(note)
   RELATIONS[interval]
 end
 
+def convert(key:, fret:)
+  shift_tuning(key:, fret:)
+    .map
+    .with_index do |note, idx|
+      string = idx + 1
+      [string, note, relation(key:, note:)]
+  end
+end
+
 def main
   root = "B"
-  key = ALL_NOTES
-    .slice_before(root)
-    .to_a # rails .split() would avoid the type fixup
-    .reverse
-    .flatten
+  key = shift_key(to: root)
 
-  converted = TUNING.map.with_index do |note, idx|
-    string = idx + 1
-    [string, note, relation(key:, note:)]
+  (0..3).map do |fret|
+    puts Terminal::Table.new(
+      title: "Fret #{fret}",
+      headings: ["Str", "Note", "#"],
+      rows: convert(key:, fret:)
+    )
   end
-
-  puts Terminal::Table.new(rows: converted)
 end
 
 main
