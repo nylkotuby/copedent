90c73a03c49ad330a63e9b8926343597a4444eb9
Lyn Kotuby <lyn.kotuby@gmail.com>
2025-01-20 23:11:47 -0500

    Organize into classes

--- lib/copedent.rb
+++ lib/copedent.rb
@@ -1,6 +1,8 @@
 #! /usr/bin/env ruby
 
 require_relative "copedent/version"
+require_relative "copedent/sheet"
+require_relative "copedent/printer"
 require "terminal-table"
 require "csv"
 
@@ -32,68 +34,4 @@ module Copedent
     "maj7", "Root", "b9", "9", "#9",
     "11", "#11", "b13", "13"
   ]
-
-  # === CODE ===
-
-  def self.shift_key(to:)
-    ALL_NOTES
-      .slice_before(to)
-      .to_a # rails .split() would avoid the type fixup
-      .reverse
-      .flatten
-  end
-
-  # returns a fret, which is several columns and rows
-  # long-term, might be better to do this less similarly to how it's done irl
-  def self.convert_fret(tuning:, key:, fret:)
-    shift_tuning(tuning:, key:, fret:)
-      .map
-      .with_index do |note, idx|
-        fret_num = idx.zero? ? fret : ""
-        [fret_num, idx + 1, note, relation(key:, note:)]
-      end
-  end
-
-  # check if the given fret has all of the required chord qualities for configured chords
-  # e.g. a major chord must have a 3, but a dom 7 must have a 3 and a b7
-  def self.has_valid_chord?(fret:, types:)
-    notes = fret.transpose[3]
-    types.any? do |type|
-      QUALITIES[type].all? { |qual| notes.include?(qual) }
-    end
-  end
-
-  def self.print_to_terminal(columns:)
-    puts "Printing valid chords to terminal..."
-
-    columns.map do |col|
-      puts Terminal::Table.new(
-        headings: ["Fr", "Str", "Note", "#"],
-        rows: col,
-      )
-    end
-  end
-
-  def self.export_csv(columns:, filename:)
-    puts "Exporting valid chords to CSV at #{CSV_FILENAME}..."
-
-    CSV.open(filename, "w") do |csv|
-      csv << ["Fret", "Str", "Note", "#"] * columns.length
-
-      columns.transpose.each { |row| csv << row.flatten }
-    end
-  end
-
-  def self.shift_tuning(tuning:, key:, fret:)
-    tuning.map do |note|
-      current_index = key.find_index(note)
-      new_index = (current_index + fret) % key.length
-      key[new_index]
-    end
-  end
-
-  def self.relation(key:, note:)
-    interval = key.find_index(note)
-    RELATIONS[interval]
-  end
 end
---
+++ lib/copedent/printer.rb
@@ -0,0 +1,25 @@
+# handles all output for the program
+module Copedent
+  class Printer
+    def self.print_to_terminal(analysis:, max_visible_frets:)
+      puts "Printing valid chords to terminal..."
+
+      analysis.take(max_visible_frets).map do |col|
+        puts Terminal::Table.new(
+          headings: ["Fr", "Str", "Note", "#"],
+          rows: col
+        )
+      end
+    end
+
+    def self.export_csv(analysis:, filename:)
+      puts "Exporting valid chords to CSV at #{CSV_FILENAME}..."
+
+      CSV.open(filename, "w") do |csv|
+        csv << ["Fret", "Str", "Note", "#"] * analysis.length
+
+        analysis.transpose.each { |row| csv << row.flatten }
+      end
+    end
+  end
+end
